3db75f052fa1ac4424b0db553a7ba3e2d010dc12
Irakli Svanidze <isvanidze@molhsa.local>
2017-10-18 20:22:27 +0400

    init commit

---
+++ virtuallib.js
@@ -0,0 +1,121 @@
+var bigInt = require("big-integer");
+
+// var readline = require('readline');
+// var rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//     terminal: false
+// });
+
+
+
+let alphabet = [
+    'ა',
+    'ბ',
+    'გ',
+    'დ',
+    'ე',
+    'ვ',
+    'ზ',
+    'თ',
+    'ი',
+    'კ',
+    'ლ',
+    'მ',
+    'ნ',
+    'ო',
+    'პ',
+    'ჟ',
+    'რ',
+    'ს',
+    'ტ',
+    'უ',
+    'ფ',
+    'ქ',
+    'ღ',
+    'ყ',
+    'შ',
+    'ჩ',
+    'ც',
+    'ძ',
+    'წ',
+    'ჭ',
+    'ხ',
+    'ჯ',
+    'ჰ',
+    ' '
+];
+
+
+// rl.on('line', function (line) {
+//     //console.log(line);
+//     console.log(virtualPage(line));
+
+// });
+
+function virtualPage(line) {
+
+    let result = '';
+    let bigstr = wordToNumber(line);
+    let current = bigInt(bigstr);
+    let p = bigInt(50).pow(10);
+    let min = bigInt(bigstr).divide(p);
+    let max = bigInt(bigstr).multiply(p);
+
+    if (min.isZero()) min = bigInt(1);
+
+    while (min.compareTo(current) != 1) {
+        let res = getWord(min.toString());
+        result += res;
+        min = min.multiply(5);
+    }
+
+    result += getWord(current.toString());
+
+    while (min.compareTo(max) != 1) {
+        let res = getWord(min.toString());
+        result += res;
+        min = min.multiply(5);
+    }
+
+    return result;
+}
+
+
+
+function wordToNumber(text) {
+    let p = alphabet.length + 1;
+    let resultNumber = bigInt(0);
+
+    for (var index = 0; index < text.length; index++) {
+        var element = text[index];
+        let alphabetIndex = alphabet.indexOf(element) + 1;
+        let pow_r = bigInt(p).pow(index);// pow(p.toString(), index);
+        let m = bigInt(alphabetIndex).multiply(pow_r); //  mul(alphabetIndex.toString(), pow_r);
+        resultNumber = resultNumber.plus(m);// resultNumber.multiply(alphabetIndex)  plus(resultNumber, m); // alphabetIndex * (p ** index);
+
+    }
+    return resultNumber.toString();
+
+}
+
+
+function getWord(x) {
+
+    let p = alphabet.length + 1;
+    let pbase = bigInt(x).toString(p);
+    //console.log(pbase);
+    let result = '';
+    for (var index = 0; index < pbase.length; index++) {
+        let item = parseInt(pbase[index], p);
+        if (item === 0) continue;
+        result = alphabet[item - 1] + result;
+
+    }
+
+    return result;
+}
+
+module.exports = {
+    getVirtualPage: virtualPage
+};
